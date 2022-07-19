75625e05734f34aaa084f1f42b5fca30bc5908dd
Deepak Sen <deepak.sen@innoeye.com>
2022-07-19 12:42:41 +0530

    firs

---
+++ apps/details/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'module-fed-details-entry',
+  template: `<module-fed-nx-welcome></module-fed-nx-welcome>`,
+})
+export class RemoteEntryComponent {}
---
+++ apps/nav/src/app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'module-fed-root',
+  template: '<router-outlet></router-outlet>',
+})
+export class AppComponent {}
